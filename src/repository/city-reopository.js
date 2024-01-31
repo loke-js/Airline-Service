388760ea9e5518b726c7c2d70d826b4732c9637c
lokendra22844 <lokendratiwari22844@gmail.com>
2024-01-31 09:20:27 +0530

    Minor Fixes

--- src/repository/city-reopository.js
+++ src/repository/city-reopository.js
@@ -30,11 +30,19 @@ class CityRepository {
 
     async updateCity(cityId, data) { // {name: "Prayagraj"}
         try {
-            const city = await City.update(data, {
-                where: {
-                    id: cityId
-                }
-            });
+            
+            // THE BELOW APPROACH ALSO WORKS BUT WILL NOT RETURN UPDATED OBJECTS
+            // if  we are using pg sql then returning:trues ban be used else not
+
+
+            // const city = await City.update(data, {
+            //     where: {
+            //         id: cityId
+            //     }
+            // });
+            const city=await City.findByPk(cityId);
+            city.name=data.name;
+            await city.save();
             return city;
         } catch (error) {
             console.log("Something went wrong in the repository layer");
