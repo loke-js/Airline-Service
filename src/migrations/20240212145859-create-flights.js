0807e853a61f3be3e75310eb4ca11f551c0a6890
lokendra22844 <lokendratiwari22844@gmail.com>
2024-02-12 21:41:15 +0530

    Added flights model

--- src/migrations/20240212145859-create-flights.js
+++ src/migrations/20240212145859-create-flights.js
@@ -11,44 +11,44 @@ module.exports = {
       },
       flightNumber: {
         type: Sequelize.STRING,
-        allowNull:false,
-        unique:true,
+        allowNull: false,
+        unique: true
       },
       airplaneId: {
         type: Sequelize.INTEGER,
-        allowNull:false,
+        allowNull: false
       },
       departureAirportId: {
         type: Sequelize.INTEGER,
-        allowNull:false,
+        allowNull: false
       },
       arrivalAirportId: {
         type: Sequelize.INTEGER,
-        allowNull:false,
+        allowNull: false
       },
       arrivalTime: {
         type: Sequelize.DATE,
-        allowNull:false,
+        allowNull: false
       },
       departureTime: {
         type: Sequelize.DATE,
-        allowNull:false,
+        allowNull: false
       },
       price: {
-        type: Sequelize.INTEGER,
-        allowNull:false,
+        type: Sequelize.INTEGER, 
+        allowNull: false
       },
       boardingGate: {
-        type: Sequelize.STRING,
-        allowNull:false,
+        type: Sequelize.STRING
+       
       },
       totalSeats: {
         type: Sequelize.INTEGER,
-        allowNull:false,
+        allowNull: false
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE 
       },
       updatedAt: {
         allowNull: false,
